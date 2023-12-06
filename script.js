9f8417c178dbd8c429c00919f7fcb8f814671d26
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-12-06 22:45:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 const sizeInfo = document.getElementById('sizeInfo');
-
+const heading = document.getElementById("heading")
 function updateSize() {
     const width = window.innerWidth;
     const height = window.innerHeight;
-    sizeInfo.textContent = `Width: ${width}, Height: ${height}`;
+    heading.textContent = `Width: ${width}, Height: ${height}`;
 }
 
 window.addEventListener('resize', updateSize);
